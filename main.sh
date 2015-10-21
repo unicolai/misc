abc50fdeeebd898f1cbef909aeb3a61520625bf5
unicolai <ulrich.nicolaisen@capgeminisogeti.dk>
2015-10-21 16:56:23 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #Run step 0. Init
-echo `Run step 0`
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+echo "${green}Run step 0${reset}"
 id -u oracle &>/dev/null || useradd oracle
 sudo -u "oracle" ./0init.sh
 ###############
